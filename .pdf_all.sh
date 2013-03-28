995dc5413b3cc4019445cd39e121452cc218b703
Lilian Besson <lbesson@ens-cachan.fr>
2013-03-28 01:49:23 +0100

    ca avance....

--- .pdf_all.sh
+++ .pdf_all.sh
@@ -11,35 +11,48 @@
 #
 # A simple script to automatize the generation fo one PDF file
 # from rST files, with rst2pdf and rst2latex -> pdflatex
+#
+#  .*.rst: are NOT used : specials and included only files
+#  *.rst: ARE used (all of them). 
 
 echo -e "$0: begin to work, on $PWD."
 
 mkdir -v --parents .build/pdf/
 
+echo -e "I will generate two PDF files (.pdf and .fromrst.pdf) for each of the following files:"
+
+ls --color=auto *.rst || exit 1
+
 read -p "[o]ui/[N]ON ? " ok
+
+echo -e "OK: I'm going to begin...."
 
 listPDFfromRSTs=""
 listTEX=""
 listPDFs=""
 
-for file in transifex*.rst
+for file in *.rst
 do
 	echo -e "\n Examining the file '$file'..."
 
-	rst2pdf -s ./.style.rst2pdf --compressed --verbose --language=fr --baseurl="http://perso.crans.org/besson/" -o ".build/pdf/${file%.rst}.fromrst.pdf" "$file" && \
+# rST -----> PDF
+	rst2pdf -s ./.style.rst2pdf --compressed --verbose --language=fr  --baseurl="http://perso.crans.org/besson/" -o ".build/pdf/${file%.rst}.fromrst.pdf" "$file" && \
 	listPDFfromRSTs="$listPDFfromRSTs .build/html/${file%.rst}.fromrst.pdf" && \
 	echo -e " '.build/pdf/${file%.rst}.pdf' well generated ...."
 
+# rST -----> LaTeX
 	rst2latex --generator --time --source-url ="http://perso.crans.org/besson/_sources/$file" --report="none" --verbose --language=fr --section-subtitles "$file" "${file%.rst}.tex" && \
 	listTEX="$listTEX ${file%.rst}.tex" && \
-	echo -e " '${file%.rst}.tex' well generated ...."
+	echo -e " '${file%.rst}.tex' well generated ...." && \
+# LaTeX ---> PDF
 	pdflatex -file-line-error -interaction=nonstopmode -output-directory=.build/pdf/ "${file%.rst}.tex" && \
 	listPDFs="$listPDFs ${file%.rst}.pdf" && \
 	pdflatex -file-line-error -interaction=nonstopmode -output-directory=.build/pdf/ "${file%.rst}.tex" && \
 	echo -e " '${file%.rst}.pdf' well generated ...." && \
-	echo -e "Erasing: ${file%.rst}.tex ${file%.rst}.log ${file%.rst}.out ${file%.rst}.aux && \
+	echo -e "Erasing: ${file%.rst}.tex ${file%.rst}.log ${file%.rst}.out ${file%.rst}.aux" && \
 	mv -fv "${file%.rst}.tex" "${file%.rst}.log" "${file%.rst}.out" "${file%.rst}.aux" /tmp/ && \
 	mv -fv ".build/pdf/${file%.rst}.log" ".build/pdf/${file%.rst}.out" ".build/pdf/${file%.rst}.aux" /tmp/
+
 	echo -e "*** Done: for the file '$file' ***"
 	echo -e "**********************************"
 done
