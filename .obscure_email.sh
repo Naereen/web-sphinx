4ec6d99424cc9619c657c39dec21377f215c7598
Lilian Besson <lbesson@ens-cachan.fr>
2015-11-10 21:51:25 +0100

    Update .obscure_email, improve performance by being lazy (only .html, .txt, .doctree)

--- .obscure_email.sh
+++ .obscure_email.sh
@@ -1,38 +1,48 @@
 #!/bin/bash
-#	__author__='Lilian BESSON'
-#	__email__='lilian DOT besson AT normale D O T fr'
-#	__version__='0.3'
-#	__date__='lundi 16/09/2013 at 19h41m'
+# Author: Lilian BESSON
+# Email: lilian DOT besson AT normale D O T fr
+# Version: 2'
+# Date: 10-11-2015
 #
-# A simple script to hide email address in Sphinx generated web pages.
+# A simple script to hide email address on documents hosted on a web server.
 #
-version='0.3'
-##LANG='en'
-##LANGUAGE='en'
-##LC_ALL='en'
+version='2'
 
-# If possible, use .color.sh (http://perso.crans.org/besson/publis/bin/.color.sh)
+# If possible, use .color.sh (http://besson.qc.to/bin/.color.sh)
 [ -f ~/.color.sh ] && ( . ~/.color.sh ; clear )
 
 GREP="grep --color=always --line-buffered"
 
-echo -e "${clear}${reset}${yellow}Starting to hide email addresses (`basename $0` $version)${reset}\n"
+echo -e "${clear}${reset}${yellow}Starting to hide email addresses (`basename $0` v${version})${reset}\n"
 
-for j in crans ens normale sfr hotmail laposte gmail live
+# for j in crans ens normale hotmail laposte gmail live
+for j in crans
 do
  echo -e "${reset}${magenta}---> For the addresses ${u}*@$j$reset$white"
 
- for t in html txt js doctree py sh ml c
- do
-  echo -e "${reset}${blue}   \\---> For the file type ${u}$t$reset$white"
-
-  for i in `find .build/ -type f -iname '*'$t`
-  do
-   ( GREP_COLOR="4;01;36" $GREP -I @$j "$i" ) >/dev/null && \
+if [ "$1" = "all" ]; then
+  echo -e "${reset}${blue}   \\---> For ${u}all file types$reset$white"
+  for i in $(find . -type f -iname '*'); do
+   ( GREP_COLOR="4;01;36" $GREP -I @$j "$i" ) &>/dev/null && \
    ( echo -e "${reset}${green}       \\--->$ For the file ${u}$i$reset$white"
-     cat "$i" | sed s/@$j/[AT]$j/  > "$i"~ | colordiff "$i" "$i"~
+     cat "$i" | sed s/@$j/[AT]$j/ > "$i"~ | diff "$i" "$i"~ | grep AT 2>/dev/null
+     /bin/mv -vf "$i"~ "$i"
+     /bin/rm -vf "$i"~ )
+  done
+else
+# for t in html txt js doctree py sh ml c xml sty tex pl rc; do
+ for t in html txt doctree; do
+  echo -e "${reset}${blue}   \\---> For the file type ${u}$t$reset$white"
+  for i in `find . -type f -iname '*'$t`; do
+   ( GREP_COLOR="4;01;36" $GREP -I @$j "$i" ) &>/dev/null && \
+   ( echo -e "${reset}${green}       \\--->$ For the file ${u}$i$reset$white"
+     cat "$i" | sed s/@$j/[AT]$j/ > "$i"~ | diff "$i" "$i"~ | grep AT 2>/dev/null
      /bin/mv -vf "$i"~ "$i"
      /bin/rm -vf "$i"~ )
   done
  done
+fi
 done
+
+exit 0
+# END
