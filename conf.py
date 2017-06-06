53468a9e2057da95807de32b5b830414bbec0c56
Lilian Besson <lbesson@ens-cachan.fr>
2017-06-06 15:11:53 +0200

    'html_use_smartypants = True' option is now deprecated.

--- conf.py
+++ conf.py
@@ -297,7 +297,7 @@ printc("<yellow>[INFO]<white> Using html_last_updated_fmt: <u>{}<U>...".format(h
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-html_use_smartypants = True
+# html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {
